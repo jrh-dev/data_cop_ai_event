e2b18c4482963dc45e6a52c34d62aa24ba4df227
James Hardy <james.hardy@airelogic.com>
2025-11-20 14:20:11 +0000

    refactored

--- hr_assistant/Home.py
+++ hr_assistant/Home.py
@@ -1,51 +1,52 @@
-import time
-from typing import List
-
 import streamlit as st
 from dotenv import load_dotenv
-from interface import Interface
-from load_policies import load_policies
+from helpers import (add_policies, cache_load_policies, create_chatbot,
+                     fake_streaming_response, tidy_title)
+
+AVATAR = "https://clippyprofilepic.net/_next/image?url=https%3A%2F%2Ftest-icons.sparkiconai.com%2Fclippys%2F3d-clippy-original.webp&w=640&q=75"
 
 load_dotenv()
+
+st.set_page_config(
+    page_title="HR Assistant Demo",
+    page_icon=None,
+    layout="wide",
+    initial_sidebar_state=None,
+    menu_items=None
+)
+
+hr_policy_chatbot = create_chatbot("hr_policies")
+
 
 if "message_history" not in st.session_state:
     st.session_state.message_history = []
 else:
     st.session_state.message_history = st.session_state.message_history[-10:]
 
-@st.cache_resource
-def _load_policies(policy_path: str):
-    return load_policies(policy_path)
-
-@st.cache_resource
-def load_and_add_policies(policies: List[str]):
-    chatbot = Interface("hr_policies")
-    for name, text in policies.items():
-        chatbot.add_policy(name, text)
-    return chatbot
-
-def fake_streaming_response(response: str):    
-    for word in response.split():
-        yield word + " "
-        time.sleep(0.05)
-
-policies = _load_policies("./Policies")
-chatbot = load_and_add_policies(policies)
+policies = cache_load_policies("./Policies")
+add_policies(hr_policy_chatbot, policies)
 
 st.header("HR helper PoC")
 
-for name, text in policies.items():
-    with st.expander(f"Policy: {name}"):
-        st.write(text)
+policies_col, chat_col = st.columns([3, 2])
 
-for m in st.session_state.message_history:
-    st.chat_message(m[0]).write(m[1])
+with policies_col:
+    st.subheader("Policies")
 
-prompt = st.chat_input("Ask")
-if prompt:
-    st.session_state.message_history.append(("user", prompt))
-    st.chat_message("user").write(prompt)
-    with st.spinner("Thinking..."):
-        chatbot_response = chatbot.ask(prompt)
-    st.chat_message("assistant").write_stream(fake_streaming_response(chatbot_response))
-    st.session_state.message_history.append(("assistant", chatbot_response))
+    for name, text in policies.items():
+        with st.expander(f"{tidy_title(name)} Policy", expanded=False):
+            st.write(text)
+
+with chat_col:
+    st.subheader("Ask virtual HR assistant")
+    prompt = st.chat_input("Ask")
+    for m in st.session_state.message_history:
+        st.chat_message(m[0]).write(m[1])
+    
+    if prompt:
+        st.session_state.message_history.append(("user", prompt))
+        st.chat_message("user").write(prompt)
+        with st.spinner("Thinking..."):
+            chatbot_response = hr_policy_chatbot.ask(prompt)
+        st.chat_message("assistant", avatar = AVATAR).write_stream(fake_streaming_response(chatbot_response))
+        st.session_state.message_history.append(("assistant", chatbot_response))
